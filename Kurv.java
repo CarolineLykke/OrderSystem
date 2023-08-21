78b7b59aaf2414a0acfa5ff417526a84a45965c6
CarolineLykke <carolinelh@live.dk>
2023-08-21 20:04:19 +0200

    kurv

--- Kurv.java
+++ Kurv.java
@@ -1,30 +1,22 @@
-/*import org.junit.Test;
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
+public class Kurv {
+    private ArrayList<String> varer;
 
-public class KurvTest {
-     private ArrayList<String> varer;
-
-     @Test
     public void Kurv() {
         varer = new ArrayList<>();
     }
 
-    @Test
     // Tilføj en vare til kurven
     public void tilfojVare(String vare) {
         varer.add(vare);
     }
 
-    @Test
     //fjern en vare i kurven
     public void fjernVare(String vare){
         varer.remove(vare);
     }
 
-    @Test
     // Metode til at udskrive varerne i kurven
     public void visVarer() {
         System.out.println("Varer i kurven:");
@@ -32,4 +24,4 @@ public class KurvTest {
             System.out.println(vare);
         }
     }
-    }*/
+}
--- Main.java
+++ Main.java
@@ -1,2 +1,15 @@
+
 public class Main {
+    public static void main(String[] args) {
+        Kurv minKurv = new Kurv();
+
+        // Tilføj varer til kurven
+        minKurv.tilfojVare("Vare 1");
+        minKurv.tilfojVare("Vare 2");
+        minKurv.tilfojVare("Vare 3");
+        minKurv.fjernVare("Vare 2");
+
+        // Vis indholdet af kurven
+        minKurv.visVarer();
+    }
 }
