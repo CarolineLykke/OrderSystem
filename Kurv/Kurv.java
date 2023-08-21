1a7c23c8b4bfd7bcf3a433a49015fb9634f5f6af
CarolineLykke <carolinelh@live.dk>
2023-08-21 16:02:11 +0200

    h

--- Kurv/Kurv.java
+++ Kurv/Kurv.java
@@ -1,4 +1,31 @@
 package Kurv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kurv {
+    private List<Item> itemList;
+
+    public ShoppingCart() {
+        itemList = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        itemList.add(item);
+    }
+
+    public void removeItem(Item item) {
+        itemList.remove(item);
+    }
+
+    public double getTotalPrice() {
+        double totalPrice = 0.0;
+
+        for (Item item : itemList) {
+            totalPrice += item.getPrice();
+        }
+
+        return totalPrice;
+    }
 }
+
